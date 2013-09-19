cc0a439747088a26074dfba835666ada368814db
Inigo <inigo@hellokitty.ites.upr.edu>
2013-09-19 11:46:39 -0400

    organization migration fixes

--- migration/LuquilloContentOrganizationMigration.php
+++ migration/LuquilloContentOrganizationMigration.php
@@ -5,7 +5,7 @@
 * Definition of LuquilloEntityOrganizationMigration.
 */
 
-class LuquilloEntityOrganizationMigration extends DeimsEntityOrganizationMigration {
+class LuquilloContentOrganizationMigration extends DeimsContentOrganizationMigration {
 
     public function __construct(array $arguments) {
     parent::__construct($arguments);
@@ -14,13 +14,13 @@ class LuquilloEntityOrganizationMigration extends DeimsEntityOrganizationMigrati
     // property, rather than a text field. So we need to rebuild the source
     // query.
     $query = $this->connection->select('content_type_person', 'ctp');
-    $query->fields('ctp', array('nid', 'field_person_organization_title'));
-    $query->condition('ctp.field_person_organization_title', '', '<>');
-    $query->groupBy('field_person_organization_title');
+    $query->fields('ctp', array('nid', 'field_person_organization_value'));
+    $query->condition('ctp.field_person_organization_value', '', '<>');
+    $query->groupBy('field_person_organization_value');
     $this->source = new MigrateSourceSQL($query);
 
     // Remap the organization title to the link field's title value.
     $this->removeFieldMapping('title');
-    $this->addFieldMapping('title', 'field_person_organization_title');
+    $this->addFieldMapping('title', 'field_person_organization_value');
     }
 }
