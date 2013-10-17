b5c88f268a647ae58c5597325b8abad5812326ee
Inigo San Gil <isangil@lternet.edu>
2013-10-16 18:54:15 -0600

    place the prefix in the hard coded queries for the Organization migration

--- migration/LuquilloContentOrganizationMigration.php
+++ migration/LuquilloContentOrganizationMigration.php
@@ -13,7 +13,7 @@ class LuquilloContentOrganizationMigration extends DeimsContentOrganizationMigra
     // LUQ uses a link field for field_person_organization that has a title
     // property, rather than a text field. So we need to rebuild the source
     // query.
-    $query = $this->connection->select('content_type_person', 'ctp');
+    $query = $this->connection->select('luq_content_type_person', 'ctp');
     $query->fields('ctp', array('nid', 'field_person_organization_value'));
     $query->condition('ctp.field_person_organization_value', '', '<>');
     $query->groupBy('field_person_organization_value');
